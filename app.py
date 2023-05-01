8858f07aa88cf9b2f532f0e402303b732690edb4
KayT256 <trattrieukhang0205@gmail.com>
2023-05-02 00:16:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
-from students import room
+from students import PY4AI_classes
 
 def main():
   st.title('PY4AI CLASS SCORES')
   tab1, tab2, tab3 = st.tabs(['Lists', 'Charts', 'Classification'])
   
-  with tab1:
-    room()
+  with tab2:
+    tab1, tab2 = st.tabs(['Number of students', 'Scores'])
+    with tab1:
+      PY4AI_classes()
+      st.success('Comment: The distribution of students in each class is nearly equal.')
+    
+
 main()
