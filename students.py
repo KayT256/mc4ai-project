af1682f5327e8c4b82f8fd02af60d8b2d003e0aa
KayT256 <trattrieukhang0205@gmail.com>
2023-05-02 00:16:50 +0700

    Update students.py

--- students.py
+++ students.py
@@ -1,7 +1,7 @@
 import plotly.express as px
-import streamlit
+import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('py4ai-score.csv')
-def room():
-  st.plotly_chart(px.pie(df, names="PYTHON-CLASS"), use_container_width=True)
+def PY4AI_classes():
+  st.plotly_chart(px.pie(df, names="PYTHON-CLASS", title='Percentages of students per PY4AI-COTAI class'))
