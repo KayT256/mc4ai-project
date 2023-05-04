79a020fa3adc0be430ec2d4205bfbf38e461e70a
KayT256 <96287500+KayT256@users.noreply.github.com>
2023-05-05 00:48:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from students import room
 
 def main():
   st.title('PY4AI CLASS SCORES')
   tab1, tab2, tab3 = st.tabs(['Lists', 'Charts', 'Classification'])
-
+  
+  with tab1:
+    room()
 main()
