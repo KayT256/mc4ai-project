f7bee32a8975d2af35adc3c46a80c58aa3af9c31
KayT256 <96287500+KayT256@users.noreply.github.com>
2023-05-05 00:58:41 +0700

    Update students.py

--- students.py
+++ students.py
@@ -1,5 +1,7 @@
 import plotly.express as px
 import streamlit
+import pandas as pd
 
+df = pd.read_csv('py4ai-score.csv')
 def room():
-  st.plotly_chart(px.pie(df[df['CountryLive'] == 'United States of America'], names="JobInterest"))
+  st.plotly_chart(px.pie(df, names="PYTHON-CLASS"))
