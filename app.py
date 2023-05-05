819f5837b4ef27701165b638bda4020f566374f6
KayT256 <trattrieukhang0205@gmail.com>
2023-05-05 23:07:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from students import PY4AI_classes
 from scores import session_scores
 from groups import score_groups
+from classification import two_features
 def main():
   st.title('PY4AI CLASS SCORES')
   tab1, tab2, tab3, tab4 = st.tabs(['Lists', 'Charts', 'Groups', 'Classification'])
@@ -11,11 +12,15 @@ def main():
     with tab21:
       PY4AI_classes()
     with tab22:
-      radio = st.radio('Sessions', ('S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'GPA'), horizontal=True)
-      session_scores(radio)
+      sessions = st.radio('Sessions', ('S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'GPA'), horizontal=True)
+      session_scores(sessions)
   with tab3:
     slider = st.slider('Number of groups', 2, 5, 3)
     score_groups(slider)
+  with tab4:
+    features = st.radio('Number of features', (2, 3), horizontal=True)
+    if features == 2:
+      two_features()
     
 
 main()
--- groups.py
+++ groups.py
@@ -9,8 +9,6 @@ df.fillna(0, inplace=True)
 
 def score_groups(a):
     kmeans = KMeans(n_clusters=a, n_init='auto')
-    min = [df.loc[i][4:13].min() for i in df.index]
-    max = [df.loc[i][4:13].max() for i in df.index]
     X = np.stack((df['S6'], df['S10'], df['GPA'])).T
     kmeans.fit(X)
     st.plotly_chart(px.scatter_3d(df, x = 'S6', y = 'S10', z = 'GPA', color = kmeans.labels_))
