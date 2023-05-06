95fee2f283a50950248d80c31d0a74a1be0b05af
KayT256 <trattrieukhang0205@gmail.com>
2023-05-06 19:02:24 +0700

    Update scores.py

--- scores.py
+++ scores.py
@@ -1,9 +1,19 @@
 import plotly.express as px
 import streamlit as st
-import pandas as pd
+from list import add_specialized_class, add_grade
 
-df = pd.read_csv('py4ai-score.csv')
+df = add_specialized_class()
+df = add_grade()
 
 def session_scores(a):
     st.plotly_chart(px.box(df, x = 'PYTHON-CLASS', y = a, color  = 'GENDER'))
     st.info('Comment: Overall, in this course, boys study better than girls')
+    st.plotly_chart(px.box(df, x = 'SPECIALIZED CLASS', y = a))
+    st.info(
+            """
+            Comment:
+            - Students from IT-specialized classes study best.
+            - Students from foreign language specialized classes study pretty weakly.
+            """
+    )
+    
